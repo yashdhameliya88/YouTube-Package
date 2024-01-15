49d8c9019cad1d38e96355220ee46916603911b4
Yash Dhameliya <yashdhameliya39@gmail.com>
2024-01-15 21:59:26 +0530

    Invoice Add

--- payscript.php
+++ payscript.php
@@ -8,6 +8,9 @@ $apiKey = "rzp_live_uKtN55KQReOrk3";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="payment.css">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <title>Document</title>
 </head>
 
@@ -19,45 +22,106 @@ include("header.php");
 ?>
     <!-- Header End-->
 
-    <!-- Main Product Start-->
-    <section class="text-gray-600 body-font" style="background-color: #f7f9fd;">
-        <div class="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center">
-            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <img class="object-cover object-center rounded" alt="hero" src="img/49 Copy.png">
-            </div>
-            <div
-                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+    <div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+    		<div class="invoice-title">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" style="text-align:center;"><b>Verify Your Details</b>
+    		</div>
+    		<hr>
+    		<div class="row">
+    			<div class="col-xs-6 text-left">
+    				<address>
+    				<strong>Customer Details:</strong><br>
+    					<b>Name: </b><?php echo $_POST['name']; ?><br>
+    					<b>Phone: </b><?php echo $_POST['mobile']; ?><br>
+                        <b>Email: </b><?php echo $_POST['email']; ?><br>
+    				</address>
+    			</div>
+    			<div class="col-xs-6 text-right">
+                <address>
+    					<strong>Order Date:</strong><br>
+    					<?php echo date("d-m-Y");?><br><br>
+    				</address>
+    			</div>
+    		</div>
+    		
+    	</div>
+    </div>
+    
+    <div class="row">
+    	<div class="col-md-12">
+    		<div class="panel panel-default">
+    			<div class="panel-heading">
+    				<h3 class="panel-title"><strong>Order summary</strong></h3>
+    			</div>
+    			<div class="panel-body">
+    				<div class="table-responsive">
+    					<table class="table table-condensed">
+    						<thead>
+                                <tr>
+        							<td><strong>Item</strong></td>
+        							<td class="text-center"><strong>Price</strong></td>
+        							<td class="text-center"><strong>Quantity</strong></td>
+        							<td class="text-right"><strong>Totals</strong></td>
+                                </tr>
+    						</thead>
+    						<tbody>
+    							<!-- foreach ($order->lineItems as $line) or some such thing here -->
+    							<tr>
+    								<td>YouTube Package</td>
+    								<td class="text-center">49.00 ₹</td>
+    								<td class="text-center">1</td>
+    								<td class="text-right">49.00 ₹</td>
+    							</tr>
+                                <tr>
+        							<td></td>
+    								<td class="text-center"> </td>
+    								<td class="text-center"></td>
+    								<td class="text-right"> ₹</td>
+    							</tr>
+                                <tr>
+            						<td></td>
+    								<td class="text-center"></td>
+    								<td class="text-center"></td>
+    								<td class="text-right"> ₹</td>
+    							</tr>
+    							<tr>
+    								<td class="thick-line"></td>
+    								<td class="thick-line"></td>
+    								<td class="thick-line text-center"><strong>Subtotal</strong></td>
+    								<td class="thick-line text-right">49.00 ₹</td>
+    							</tr>
+    							<tr>
+    								<td class="no-line"></td>
+    								<td class="no-line"></td>
+    								<td class="no-line text-center"><strong>TAX RATE</strong></td>
+    								<td class="no-line text-right">00.00 ₹</td>
+    							</tr>
+    							<tr>
+    								<td class="no-line"></td>
+    								<td class="no-line"></td>
+    								<td class="no-line text-center"><strong>Total</strong></td>
+    								<td class="no-line text-right">49.00 ₹</td>
+    							</tr>
+                                <tr>
+    								<td class="no-line"></td>
+    								<td class="no-line"></td>
+    								<td class="no-line"></td>
+    								
+    								<td class="no-line text-right"><button id="rzp-button1" type="submit" value="Pay Now"
+                    class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" style="width: 100%;">Pay Now</button></td>
+    							</tr>
+    						</tbody>
+    					</table>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</div>
 
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"><b>Verify Your Details</b> </h1>
-                <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion
-                    axe</p>
-                    <div class="relative mb-4">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                        <input type="text" id="name" name="name"
-                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            require>
-                    </div>
-                    <div class="relative mb-4">
-                        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                        <input type="email" id="email" name="email"
-                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            require>
-                    </div>
-                    <div class="relative mb-4">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Mobile No.</label>
-                        <input type="number" id="mobile" name="mobile"
-                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            require>
-                    </div>
-                        <button id="rzp-button1" type="submit" value="Pay Now" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Payment 123d</button>
-
-            </div>
-        </div>
-        </div>
-    </section>
-    <!-- Main Product End-->
-
-<?php
+    <?php
 include("footer.php");
 ?>
 
@@ -75,7 +139,7 @@ var options = {
     "amount": "100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
     "currency": "INR",
     "name": "Technical House", //your business name
-    "description": "Do your Payment TH...",
+    "description": "",
     "image": "https://yt3.googleusercontent.com/yti/AGOGRCqpMYAw_JXbe741H1TG2B6ckH7cirv5S9FtCe1lHQ=s88-c-k-c0x00ffffff-no-rj",
     "id": "<?php echo 'OID'.rand(10,100).'YTPKG'; ?>", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
     "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
