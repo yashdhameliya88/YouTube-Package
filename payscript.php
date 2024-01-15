17bf48f0a3a6e13ec4374b1e6c7815883222d599
Yash Dhameliya <yashdhameliya39@gmail.com>
2024-01-15 21:04:46 +0530

    payscript update

--- payscript.php
+++ payscript.php
@@ -1,7 +1,72 @@
 <?php
 $apiKey = "rzp_live_uKtN55KQReOrk3";
 ?>
-<button id="rzp-button1">Payment 123d</button>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="payment.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- Header Start-->
+    <?php
+include("header.php");
+?>
+    <!-- Header End-->
+
+    <!-- Main Product Start-->
+    <section class="text-gray-600 body-font" style="background-color: #f7f9fd;">
+        <div class="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="img/49 Copy.png">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"><b>Verify Your Details</b> </h1>
+                <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion
+                    axe</p>
+                    <div class="relative mb-4">
+                        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                        <input type="text" id="name" name="name"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            require>
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+                        <input type="email" id="email" name="email"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            require>
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="name" class="leading-7 text-sm text-gray-600">Mobile No.</label>
+                        <input type="number" id="mobile" name="mobile"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            require>
+                    </div>
+                        <button id="rzp-button1" type="submit" value="Pay Now" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Payment 123d</button>
+
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- Main Product End-->
+
+<?php
+include("footer.php");
+?>
+
+
+</body>
+
+</html>
+
+
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 
 <script>
@@ -27,7 +92,7 @@ var options = {
     }
 };
 var rzp1 = new Razorpay(options);
-document.getElementById('rzp-button1').onclick = function(e){
+document.getElementById('rzp-button1').onclick = function(e) {
     rzp1.open();
     e.preventDefault();
 }
