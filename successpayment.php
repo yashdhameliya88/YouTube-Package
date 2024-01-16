11f1a9f6e99e3d927de4c4214cdd01376b959581
Yash Dhameliya <yashdhameliya39@gmail.com>
2024-01-16 23:35:35 +0530

    Successpage Add

--- successpayment.php
+++ successpayment.php
@@ -1,25 +1,29 @@
-
 <?php
 include('db.php');
 
-if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    
-    // Retrieve data
-    $sql = "SELECT * FROM tbluser WHERE id = $id";
-    $result = $conn->query($sql);
+
+    // Check if password has been generated
+    $checkSql = "SELECT * FROM tbluser WHERE id = $id AND password_generated = 0";
+    $result = $conn->query($checkSql);
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
+        $name = $row['name'];
         $email = $row['email'];
         $password = generateRandomPassword();
 
-        // Update password in the database
-        $updateSql = "UPDATE tbluser SET password = '$password' WHERE id = $id";
+        // Update password and set the flag
+        $updateSql = "UPDATE tbluser SET password = '$password', password_generated = 1 WHERE id = $id";
         $conn->query($updateSql);
     } else {
-        echo "No data found";
-        exit();
+        // Password has already been generated
+        $selectSql = "SELECT * FROM tbluser WHERE id = $id";
+        $result = $conn->query($selectSql);
+        $row = $result->fetch_assoc();
+        $email = $row['email'];
+        $password = $row['password'];
     }
 } else {
     echo "Invalid request";
@@ -37,39 +41,216 @@ function generateRandomPassword($length = 8) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Document</title>
 </head>
+
 <body>
-      <!-- Header Start-->
-      <?php
+    <!-- Header Start-->
+    <?php
 include("header.php");
 ?>
-<!-- Main Product Start-->
-<section class="text-gray-600 body-font" style="background-color: #f7f9fd;">
-    <div class="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center">
-      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-        <img class="object-cover object-center rounded" alt="hero" src="img/49 Copy.png">
-      </div>
-      <div
-        class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"><b>Payment successful</b>
-        </h1>
-        <b><h3>Here is your ID and Password please save it.</h3></b>
-        <br class="hidden lg:inline-block">
-        <p>ID: <?php echo $email; ?></p>
-    <p>Password: <?php echo $password; ?></p>
-        <div class="flex justify-center">
-          <a class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg button1"
-            href="https://rzp.io/l/ytpack" target="_blank">Buy Now</a>
+    <!-- Main Product Start-->
+    <section class="text-gray-600 body-font" style="background-color: #d7d7d7;">
+        <div class="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center">
+            <!-- Content Start -->
+            <table cellpadding="0" cellspacing="0" cols="1" bgcolor="#d7d7d7" align="center" style="max-width: 600px;">
+                <tr bgcolor="#d7d7d7">
+                    <td
+                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                    </td>
+                </tr>
+
+                <!-- This encapsulation is required to ensure correct rendering on Windows 10 Mail app. -->
+                <tr bgcolor="#d7d7d7">
+                    <td
+                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                        <!-- Seperator Start -->
+                        <table cellpadding="0" cellspacing="0" cols="1" bgcolor="#d7d7d7" align="center"
+                            style="max-width: 600px; width: 100%;">
+                            <tr bgcolor="#d7d7d7">
+                                <td height="30"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                        </table>
+                        <!-- Seperator End -->
+
+                        <!-- Generic Pod Left Aligned with Price breakdown Start -->
+                        <table align="center" cellpadding="0" cellspacing="0" cols="3" bgcolor="white"
+                            class="bordered-left-right"
+                            style="border-left: 10px solid #d7d7d7; border-right: 10px solid #d7d7d7; max-width: 600px; width: 100%;">
+                            <tr height="50">
+                                <td colspan="3"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr align="center">
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td class="text-primary"
+                                    style="color: #F16522; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                    <img src="http://dgtlmrktng.s3.amazonaws.com/go/emails/generic-email-template/tick.png"
+                                        alt="GO" width="50"
+                                        style="border: 0; font-size: 0; margin: 0; max-width: 100%; padding: 0;">
+                                </td>
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr height="17">
+                                <td colspan="3"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr align="center">
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td class="text-primary"
+                                    style="color: #F16522; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                    <h1
+                                        style="color: #F16522; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 30px; font-weight: 700; line-height: 34px; margin-bottom: 0; margin-top: 0;">
+                                        Payment received</h1>
+                                </td>
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr height="30">
+                                <td colspan="3"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr align="left">
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">
+                                        Hi <?php echo $name; ?>,
+                                    </p>
+                                </td>
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr height="10">
+                                <td colspan="3"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr align="left">
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">
+                                        Your transaction was successful!</p>
+                                    <br>
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0; ">
+                                        <strong>Payment Details:</strong><br />
+
+                                        Amount: 49.00 ₹ <br />
+                                    </p><br>
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0; ">
+                                        <strong>Your Login ID and Password:</strong><br />
+
+                                        ID: <b style="color:blue;"><?php echo $email; ?></b> <br />             
+                                        Password: <b style="color:blue;"><?php echo $password; ?></b>
+                                    </p>
+                                    <br>
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">
+                                        We advise to keep this as screenshoot for future reference.&nbsp;&nbsp;&nbsp;&nbsp;<br />
+                                    </p>
+                                </td>
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr height="30">
+                                <td
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td
+                                    style="border-bottom: 1px solid #D3D1D1; color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr height="30">
+                                <td colspan="3"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                            <tr align="center">
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                                <td
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">
+                                        <strong>Login to Download YouTube Package</strong>
+                                    </p><br>
+                                    <a class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg button1"
+                        href="https://rzp.io/l/ytpack" target="_blank">Login</a>
+                                    <p
+                                        style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">
+                                    </p>
+                                </td>
+                                <td width="36"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+
+                            <tr height="50">
+                                <td colspan="3"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+
+                        </table>
+                        <!-- Generic Pod Left Aligned with Price breakdown End -->
+
+                        <!-- Seperator Start -->
+                        <table cellpadding="0" cellspacing="0" cols="1" bgcolor="#d7d7d7" align="center"
+                            style="max-width: 600px; width: 100%;">
+                            <tr bgcolor="#d7d7d7">
+                                <td height="50"
+                                    style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                </td>
+                            </tr>
+                        </table>
+                        <!-- Seperator End -->
+
+                    </td>
+                </tr>
+            </table>
+
+
         </div>
-      </div>
-    </div>
-  </section>
-<!-- Main Product End-->
-  
+    </section>
+    <!-- Main Product End-->
+
+    <!-- Footer Start -->
+<?php
+include "footer.php";
+?>
+    <!-- Footer End -->
 </body>
+
 </html>
